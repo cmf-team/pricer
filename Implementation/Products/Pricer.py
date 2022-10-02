3eb1b2129b6a0afd047fbbeb1db722d086cb233e
DunaevYaroslav <70894113+DunaevYaroslav@users.noreply.github.com>
2022-10-02 22:48:45 +0300

    Add files via upload
    Fix first version

---
+++ Implementation/Products/Pricer.py
@@ -0,0 +1,30 @@
+import CashFlow
+from abc import abstractmethod, ABC
+from datetime import date
+
+
+class Pricer(ABC):
+    @abstractmethod
+    def init(self):
+        pass
+
+    @abstractmethod
+    def getValuationDate(self) -> date:
+        pass
+
+    @abstractmethod
+    def getDiscountFactor(self, paymentDate: date) -> date:
+        pass
+
+    @abstractmethod
+    def getCallOptionBasePrice(
+        self,
+        underlying: str,
+        strike: float,
+        maturityDate: date
+        ) -> float:
+        pass
+
+    @abstractmethod
+    def CashFlowBasePrice(self, priceElement: CashFlow) -> float:
+        pass
