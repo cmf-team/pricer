4296d3c4c51f93bead7f0767d6f5a1829e34be9f
MikhailBespalov <bespalov.ml@gmail.com>
2022-10-11 11:24:45 +0300

    MoexQuoteProvider
    Third attempt to make a commit of MoexQuoteProvider class. Both tests and the class itself transfered to file MoexQuoteProvider.py

--- Implementation/Markets/MoexQuoteProvider.py
+++ Implementation/Markets/MoexQuoteProvider.py
@@ -107,3 +107,19 @@ class MoexQuoteProvider(QuoteProvider):
             self.__chart.update(layout_xaxis_rangeslider_visible=False)
             self.__chart.show()
         pass
+
+
+
+# simple test for getQuotes method
+sampleQuoteObj = MoexQuoteProvider('TQBR')
+sampleDates = [date(2022, 1, 9), date(2022, 2, 10), date(2022, 10, 3),
+    date(2022, 10, 10)]
+
+samplePrices = sampleQuoteObj.getQuotes('GAZP', sampleDates)
+for i in range(0, len(sampleDates)):
+    print(sampleDates[i], samplePrices[i])
+
+# simple test for getChart method
+sampleChartObj=MoexQuoteProvider('TQBR')
+sampleQuoteObj.getChart('GAZP',date(2022,1,1),date(2022,10,10))
+sampleQuoteObj.getChart('SBER',date(2022,1,1),date(2022,10,10))
