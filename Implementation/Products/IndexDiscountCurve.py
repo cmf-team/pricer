83dd8d38351ea73df1d9184ff5effa0aca960621
Igor <igorlaryush@yandex.ru>
2022-10-15 16:39:32 +0300

    fix remarks second time

--- Implementation/Products/IndexDiscountCurve.py
+++ Implementation/Products/IndexDiscountCurve.py
@@ -1,19 +1,9 @@
 import math
-from abc import ABC, abstractmethod
 from datetime import date
 from typing import List
 
+from Products.DiscountCurve import DiscountCurve
 from Products.QuoteProvider import QuoteProvider
-
-
-class DiscountCurve(ABC):
-    @abstractmethod
-    def getDiscountFactor(self, paymentDate: date):
-        pass
-
-    @abstractmethod
-    def get_valuationDate(self) -> date:
-        pass
 
 
 class IndexDiscountCurve(DiscountCurve):
@@ -24,18 +14,18 @@ class IndexDiscountCurve(DiscountCurve):
         tickers: List[str],
         market: QuoteProvider
     ) -> None:
-        self.valuationDate = valuationDate
-        self.durations = self.tenorsToAct(tenors)
-        self.rates = [
+        self.__valuationDate = valuationDate
+        self.__durations = self.tenorsToAct(tenors)
+        self.__rates = [
             i[0] / 100 for i in
-            [market.getQuotes(ticker, [valuationDate]) for ticker in tickers]
+            [market.getQuotes(ticker, [self.__valuationDate]) for ticker in tickers]
         ]
 
     def getDiscountFactor(self, paymentDate: date) -> float:
-        timeToPayment = (paymentDate - self.valuationDate).days / 365
+        timeToPayment = (paymentDate - self.__valuationDate).days / 365
         rate = 0
-        for i in range(len(self.durations)):
-            if timeToPayment > self.durations[i] or len(self.durations) == 1:
+        for i in range(len(self.__durations)):
+            if timeToPayment > self.__durations[i] or len(self.__durations) == 1:
                 rate = self.__interpolate(timeToPayment, i)
                 break
 
@@ -43,39 +33,39 @@ class IndexDiscountCurve(DiscountCurve):
         return discontFactor
 
     def __interpolate(self, timeToPayment: float, rate_position: int) -> float:
-        if rate_position < len(self.rates) - 1 and len(self.rates) > 1:
+        if rate_position < len(self.__rates) - 1 and len(self.__rates) > 1:
             output = (
-                self.rates[rate_position] +
-                (timeToPayment - self.durations[rate_position]) *
+                self.__rates[rate_position] +
+                (timeToPayment - self.__durations[rate_position]) *
                 (
-                    (self.rates[rate_position+1] - self.rates[rate_position]) /
+                    (self.__rates[rate_position+1] - self.__rates[rate_position]) /
                     (
-                        self.durations[rate_position+1] -
-                        self.durations[rate_position]
+                        self.__durations[rate_position+1] -
+                        self.__durations[rate_position]
                     )
                 )
             )
-        elif rate_position >= len(self.rates) - 1 and len(self.rates) > 1:
-            output = self.rates[-1] + (timeToPayment - self.durations[-1]) * (
-                (self.rates[-2] - self.rates[-1]) /
-                (self.durations[-2] - self.durations[-1])
+        elif rate_position >= len(self.__rates) - 1 and len(self.__rates) > 1:
+            output = self.__rates[-1] + (timeToPayment - self.__durations[-1]) * (
+                (self.__rates[-2] - self.__rates[-1]) /
+                (self.__durations[-2] - self.__durations[-1])
             )
-        elif len(self.rates) == 1:
-            output = self.rates[0]
+        elif len(self.__rates) == 1:
+            output = self.__rates[0]
         return output
 
     def tenorsToAct(self, durations: List[str]) -> List:
         act_durations = []
         for duration in durations:
-            if duration[-1] == 'D':
+            if duration.endswith('D'):
                 act_durations.append(int(duration[:-1]) / 365)
-            elif duration[-1] == 'W':
+            elif duration.endswith('W'):
                 act_durations.append(int(duration[:-1]) * 7 / 365)
-            elif duration[-1] == 'M':
+            elif duration.endswith('M'):
                 act_durations.append(int(duration[:-1]) * 30 / 365)
-            elif duration[-1] == 'Y':
+            elif duration.endswith('Y'):
                 act_durations.append(int(duration[:-1]))
         return act_durations
 
-    def get_valuationDate(self) -> date:
-        return self.valuationDate
+    def getValuationDate(self) -> date:
+        return self.__valuationDate
