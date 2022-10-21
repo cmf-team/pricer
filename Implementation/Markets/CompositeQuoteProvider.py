895759e22995bf77b603831cdb8a1a65980e0eaa
bekhzod <erifieder@gmail.com>
2022-10-21 11:42:00 +0500

    Create CompositeQuoteProvider Which works based on Composite pattern

---
+++ Implementation/Markets/CompositeQuoteProvider.py
@@ -0,0 +1,29 @@
+from msilib.schema import Error
+from Products.QuoteProvider import QuoteProvider
+from typing import List, Iterable
+from datetime import date
+
+
+class CompositeQuoteProvider(QuoteProvider):
+    def __init__(
+        self,
+        components: Iterable[QuoteProvider]
+    ) -> None:
+        self.components = components
+
+    def getQuotes(
+        self,
+        ticker: str,
+        observationDates: List[date]
+    ) -> List[float]:
+        for component in range(len(self.components) - 1):
+            if self.components[component].getQuotes(
+                ticker, observationDates
+            ) == self.components[component].getQuotes(
+                ticker, observationDates
+            ):
+                return self.components[component].getQuotes(
+                    ticker, observationDates
+                )
+            else:
+                raise ValueError
