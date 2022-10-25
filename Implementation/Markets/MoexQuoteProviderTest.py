e5385927e663ec69aa5e65665a4627a6a852690e
Leonid Konovalov <leonid.knv@gmail.com>
2022-10-25 21:52:08 +0300

    Refine documentation

--- Implementation/Markets/MoexQuoteProviderTest.py
+++ Implementation/Markets/MoexQuoteProviderTest.py
@@ -6,8 +6,8 @@ import apimoex
 
 from Markets.MoexQuoteProvider import MoexQuoteProvider
 
-
-# Quotes expected to be returned by apimoex.get_board_history for GAZP ticker
+# Quote data expected to be returned
+# by apimoex.get_board_history for GAZP ticker
 sampleQuoteData = [
     {'TRADEDATE': '2022-01-07', 'CLOSE': None},
     {'TRADEDATE': '2022-02-10', 'CLOSE': 328.93},
@@ -21,13 +21,16 @@ class MoexQuoteProviderTest(TestCase):
     def setUp(self) -> None:
         self.__testedQuoteProvider = MoexQuoteProvider("TQBR")
 
+    # Patch function to avoid dependence from external service in unit test.
     @patch.object(apimoex, apimoex.get_board_history.__name__)
     def testQuotes(self, mock_get_board_history):
         sampleDates = [
             date(2022, 1, 7),
-            date(2022, 1, 10),  # Date not in sampleQuoteData
+            date(2022, 1, 10),  # Date not in sampleQuoteData, None expected.
             date(2022, 2, 10),
-            date(2022, 10, 10)  # Previous date is sampleQuoteData skipped
+            # date(2022, 10, 3),
+            # Omit observation date, sampleQuoteData element must be skipped.
+            date(2022, 10, 10)
         ]
         mock_get_board_history.return_value = sampleQuoteData
         self.assertEqual(
