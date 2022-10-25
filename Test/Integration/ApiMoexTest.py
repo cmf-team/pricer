21a3d87202d1791a992ce7fd30cd13e1d52ccc9d
Leonid Konovalov <leonid.knv@gmail.com>
2022-10-25 21:51:47 +0300

    Avoid repeated service calls

--- Test/Integration/ApiMoexTest.py
+++ Test/Integration/ApiMoexTest.py
@@ -10,16 +10,14 @@ class ApiMoexTest(TestCase):
 
     def testStockQuotesFeed(self):
         with requests.Session() as session:
+            serviceQuotes = apimoex.get_board_history(
+                session,
+                'GAZP',
+                '2022-01-07',
+                '2022-10-10',
+                ('TRADEDATE', 'CLOSE'),
+                'TQBR'
+            )
             for expectedQuote in sampleQuoteData:
                 with self.subTest(f"GAZP @ {expectedQuote['TRADEDATE']}"):
-                    self.assertIn(
-                        expectedQuote,
-                        apimoex.get_board_history(
-                            session,
-                            'GAZP',
-                            '2022-01-07',
-                            '2022-10-10',
-                            ('TRADEDATE', 'CLOSE'),
-                            'TQBR'
-                        )
-                    )
+                    self.assertIn(expectedQuote, serviceQuotes)
