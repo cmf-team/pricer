2a6376d4ae1b372e14c028d1815e0c17976186e7
Igor <igorlaryush@yandex.ru>
2022-10-25 16:26:45 +0300

    fix interpolation and remarks

--- Implementation/Products/IndexDiscountCurve.py
+++ Implementation/Products/IndexDiscountCurve.py
@@ -15,63 +15,61 @@ class IndexDiscountCurve(DiscountCurve):
         market: QuoteProvider
     ) -> None:
         self.__valuationDate = valuationDate
-        self.__durations = self.tenorsToAct(tenors)
+        self.__durations = self.__tenorToDuration(tenors)
         self.__rates = [
             i[0] / 100 for i in
-            [market.getQuotes(ticker, [self.__valuationDate]) for ticker in tickers]
+            [market.getQuotes(
+                ticker,
+                [self.__valuationDate],
+            ) for ticker in tickers]
         ]
 
     def getDiscountFactor(self, paymentDate: date) -> float:
         timeToPayment = (paymentDate - self.__valuationDate).days / 365
-        rate = 0
+        rate = 0.
 
         if len(self.__durations) == 1:
             rate = self.__interpolate(timeToPayment, 0)
-        elif timeToPayment > self.__durations[-1]:
-            rate = self.__interpolate(timeToPayment, len(self.__durations) - 1)
+        elif timeToPayment >= self.__durations[-1]:
+            rate = self.__interpolate(timeToPayment, len(self.__durations) - 2)
         else:
             for i in range(1, len(self.__durations)):
                 if timeToPayment < self.__durations[i]:
                     rate = self.__interpolate(timeToPayment, i - 1)
                     break
-        
+
         discontFactor = math.exp(-rate*timeToPayment)
         return discontFactor
 
-    def __interpolate(self, timeToPayment: float, rate_position: int) -> float:
-        if rate_position < len(self.__rates) - 1 and len(self.__rates) > 1:
-            output = (
-                self.__rates[rate_position] +
-                (timeToPayment - self.__durations[rate_position]) *
+    def __interpolate(self, timeToPayment: float, ratePosition: int) -> float:
+        result = (
+            self.__rates[ratePosition] +
+            (timeToPayment - self.__durations[ratePosition]) *
+            (
                 (
-                    (self.__rates[rate_position+1] - self.__rates[rate_position]) /
-                    (
-                        self.__durations[rate_position+1] -
-                        self.__durations[rate_position]
-                    )
+                    self.__rates[ratePosition + 1] -
+                    self.__rates[ratePosition]
+                ) /
+                (
+                    self.__durations[ratePosition+1] -
+                    self.__durations[ratePosition]
                 )
             )
-        elif rate_position >= len(self.__rates) - 1 and len(self.__rates) > 1:
-            output = self.__rates[-1] + (timeToPayment - self.__durations[-1]) * (
-                (self.__rates[-2] - self.__rates[-1]) /
-                (self.__durations[-2] - self.__durations[-1])
-            )
-        elif len(self.__rates) == 1:
-            output = self.__rates[0]
-        return output
+        )
+        return result
 
-    def tenorsToAct(self, durations: List[str]) -> List:
-        act_durations = []
-        for duration in durations:
-            if duration.endswith('D'):
-                act_durations.append(int(duration[:-1]) / 365)
-            elif duration.endswith('W'):
-                act_durations.append(int(duration[:-1]) * 7 / 365)
-            elif duration.endswith('M'):
-                act_durations.append(int(duration[:-1]) * 30 / 365)
-            elif duration.endswith('Y'):
-                act_durations.append(int(duration[:-1]))
-        return act_durations
+    def __tenorToDuration(self, tenors: List[str]) -> List:
+        actDurations = []
+        for tenor in tenors:
+            if tenor.endswith('D'):
+                actDurations.append(int(tenor[:-1]) / 365)
+            elif tenor.endswith('W'):
+                actDurations.append(int(tenor[:-1]) * 7 / 365)
+            elif tenor.endswith('M'):
+                actDurations.append(int(tenor[:-1]) * 30 / 365)
+            elif tenor.endswith('Y'):
+                actDurations.append(int(tenor[:-1]))
+        return actDurations
 
     def getValuationDate(self) -> date:
         return self.__valuationDate
--- Implementation/Products/IndexDiscountCurveTest.py
+++ Implementation/Products/IndexDiscountCurveTest.py
@@ -1,19 +1,19 @@
 from unittest import TestCase
 from datetime import date
-from typing import List
+from typing import List, Dict, Optional
 from Products.IndexDiscountCurve import IndexDiscountCurve
 from Products.QuoteProvider import QuoteProvider
 
 
-class QuoteProviderDisc(QuoteProvider):
-    def __init__(self, ticker_values: map):
+class QuoteProviderStub(QuoteProvider):
+    def __init__(self, ticker_values: Dict):
         self.__tickerValues = ticker_values
 
     def getQuotes(
         self,
         ticker: str,
         observationDates: List[date]
-    ) -> List[float]:
+    ) -> Optional[List[float]]:
 
         if observationDates == [date(2022, 9, 1)]:
             for key in self.__tickerValues.keys():
@@ -22,6 +22,8 @@ class QuoteProviderDisc(QuoteProvider):
         else:
             raise NotImplementedError()
 
+        return None
+
 
 class VanillaStructuredProductTest(TestCase):
     def setUp(self):
@@ -29,50 +31,55 @@ class VanillaStructuredProductTest(TestCase):
             valuationDate=date(2022, 9, 1),
             tenors=['1D', '1W', '1M', '1Y'],
             tickers=["RATE1D", "RATE1W", "RATE1M", "RATE1Y"],
-            market=QuoteProviderDisc({'RATE1D': 0.5, 'RATE1W': 1.3, 'RATE1M': 3.6, 'RATE1Y': 12.9}),
+            market=QuoteProviderStub({
+                'RATE1D': 0.5,
+                'RATE1W': 1.3,
+                'RATE1M': 3.6,
+                'RATE1Y': 12.9,
+            }),
         )
 
     def testDayInterpolation(self):
-        self.assertEqual(0.9999, round(
-            self.__testedCurve.getDiscountFactor(date(2022, 9, 5)), 4)
+        self.assertEqual(0.999901, round(
+            self.__testedCurve.getDiscountFactor(date(2022, 9, 5)), 6)
         )
 
     def testDayPointValue(self):
         self.assertEqual(1, round(
-            self.__testedCurve.getDiscountFactor(date(2022, 9, 1)), 4)
+            self.__testedCurve.getDiscountFactor(date(2022, 9, 1)), 6)
         )
 
     def testWeekInterpolation(self):
-        self.assertEqual(0.9996, round(
-            self.__testedCurve.getDiscountFactor(date(2022, 9, 10)), 4)
+        self.assertEqual(0.99963, round(
+            self.__testedCurve.getDiscountFactor(date(2022, 9, 10)), 6)
         )
 
     def testWeekPointValue(self):
-        self.assertEqual(0.9998, round(
-            self.__testedCurve.getDiscountFactor(date(2022, 9, 8)), 4)
+        self.assertEqual(0.999751, round(
+            self.__testedCurve.getDiscountFactor(date(2022, 9, 8)), 6)
         )
 
     def testMonthInterpolation(self):
-        self.assertEqual(0.9959, round(
-            self.__testedCurve.getDiscountFactor(date(2022, 10, 10)), 4)
+        self.assertEqual(0.995895, round(
+            self.__testedCurve.getDiscountFactor(date(2022, 10, 10)), 6)
         )
 
     def testMonthPointValue(self):
-        self.assertEqual(0.997, round(
-            self.__testedCurve.getDiscountFactor(date(2022, 10, 1)), 4)
+        self.assertEqual(0.997045, round(
+            self.__testedCurve.getDiscountFactor(date(2022, 10, 1)), 6)
         )
 
     def testYearInterpolation(self):
-        self.assertEqual(0.9773, round(
-                self.__testedCurve.getDiscountFactor(date(2023, 1, 10)), 4)
+        self.assertEqual(0.977278, round(
+                self.__testedCurve.getDiscountFactor(date(2023, 1, 10)), 6)
         )
 
     def testYearPointValue(self):
-        self.assertEqual(0.8619, round(
-            self.__testedCurve.getDiscountFactor(date(2023, 10, 1)), 4)
+        self.assertEqual(0.878974, round(
+            self.__testedCurve.getDiscountFactor(date(2023, 9, 1)), 6)
         )
-    
+
     def testExtrapolation(self):
-        self.assertEqual(0.1765, round(
-            self.__testedCurve.getDiscountFactor(date(2026, 9, 1)), 4)
+        self.assertEqual(0.176535, round(
+            self.__testedCurve.getDiscountFactor(date(2026, 9, 1)), 6)
         )
