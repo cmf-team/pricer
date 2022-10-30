b4f622a03331be664ddcae9f1cdd1d7d061c808e
Leonid Konovalov <leonid.knv@gmail.com>
2022-10-31 00:35:33 +0300

    Implement quote aggregation

--- Implementation/Markets/CompositeQuoteProvider.py
+++ Implementation/Markets/CompositeQuoteProvider.py
@@ -8,5 +8,19 @@ class CompositeQuoteProvider(QuoteProvider):
         self.__components = components
 
     def getQuotes(self, ticker: str, observationDates: List[date]) -> List[float]:
+        result = [None] * len(observationDates)
         for component in self.__components:
-            quote = component.getQuotes(ticker, observationDates)
+            componentQuotes = component.getQuotes(ticker, observationDates)
+            for quoteIndex in range(len(observationDates)):
+                if componentQuotes[quoteIndex] is not None:
+                    if result[quoteIndex] is None:
+                        result[quoteIndex] = componentQuotes[quoteIndex]
+                    else:
+                        if result[quoteIndex] != componentQuotes[quoteIndex]:
+                            raise ValueError(
+                                f"Inconsistent quotes {result[quoteIndex]}"
+                                f"and {componentQuotes[quoteIndex]} for "
+                                f"{ticker} on {observationDates[quoteIndex]} "
+                                f"returned by different components."
+                            )
+        return result
